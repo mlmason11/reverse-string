4f5fc8b884777bb2442928afd3fe20552a6c6508
Max <mlmason11@gmail.com>
2023-07-11 22:40:17 -0400

    Finalized reverse string algorithm

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let rts = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    rts += str[i];
+  }
+  return rts;
 }
 
 if (require.main === module) {
